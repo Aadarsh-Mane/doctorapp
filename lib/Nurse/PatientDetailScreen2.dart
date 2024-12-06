889a94c0be654ca3398100cfe94d79627415c8c2
aadarsh <onlyaddy68@gmail.com>
2024-12-06 15:49:42 +0530

    removed warning

--- lib/Nurse/PatientDetailScreen2.dart
+++ lib/Nurse/PatientDetailScreen2.dart
@@ -8,7 +8,6 @@ import 'package:flutter/material.dart';
 import 'package:doctorapp/models/getNewPatientModel.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class PatientDetailScreen2 extends StatefulWidget {
   final Patient1 patient;
